9a75f4272af5de04958c78c2d792d80dcaddb80b
FUJI1229 <fjkwsht@gmail.com>
2025-06-05 15:39:29 +0900

    increase max_token

--- planner.py
+++ planner.py
@@ -62,7 +62,7 @@ class Base_Planner(ABC):
                 response = self.client.chat.completions.create(
                     model=self.model,
                     messages=messages,
-                    max_tokens=120,
+                    max_tokens=200,
                     temperature=0.9,
                     top_p=0.9,
                 )
